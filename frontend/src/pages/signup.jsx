c86f97535de69d73789c68079f351bc0e3f992a3
Starfoxxy <athenajacob97@gmail.com>
2025-07-21 23:57:40 -0400

    Rendered Signup

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -80,9 +80,8 @@ export default function Signup(){
    <div className="bg-gray-100 flex items-center justify-center min-h-screen">
       <div className="max-w-md w-full bg-white p-6 rounded-lg shadow-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Signup</h2>
-          {/* Render SignUpAlert here... */}
-          {/* Render SignupForm here... */}
-        <p className="mt-4 text-center text-sm">Have an account? <Link to="/login" className="text-blue-500 hover:underline">Login</Link></p>
+        <SignupAlert alert={alert} showAlert={showAlert} />
+        <SignupForm register={register} handleSubmit={handleSubmit} signupUser={signupUser} />        <p className="mt-4 text-center text-sm">Have an account? <Link to="/login" className="text-blue-500 hover:underline">Login</Link></p>
       </div>
     </div> 
   )
