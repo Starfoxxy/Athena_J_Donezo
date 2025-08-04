8d1afb95c7a5b0fd8bf7531cca8817bba4d401ac
Starfoxxy <athenajacob97@gmail.com>
2025-08-04 16:25:08 -0400

    Added main layout

--- frontend/src/layouts/main-layout.jsx
+++ frontend/src/layouts/main-layout.jsx
@@ -1,3 +1,27 @@
-export default function MainLayout(){
+import { Outlet, Link, useNavigate } from 'react-router-dom';
+import supabase from '../client';
 
+export default function MainLayout(){
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    navigate('/login');
+  };
+
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <Link to="/" className="btn btn-ghost text-xl">Donezo</Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li><button onClick={handleLogout} className="btn btn-link">Logout</button></li>
+          </ul>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
 }
