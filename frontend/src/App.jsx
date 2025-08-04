3d32b8171fe4c91c2fdfdd5e694f92c166998072
Starfoxxy <athenajacob97@gmail.com>
2025-08-04 16:44:09 -0400

    Added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,23 @@
-import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
-import { QueryClientProvider, QueryClient} from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import MainLayout from "./layouts/main-layout";
 import ProtectedRoute from "./components/protected-route";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 import Todos from "./pages/todos";
-import { useEffect } from "react";
 
 const client = new QueryClient();
-
-function RedirectToLogin() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/login")
-  }, [])
-
-  return <></>
-}
 
 function App() {
   return (
     <QueryClientProvider client={client}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<RedirectToLogin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/todos" element={<MainLayout />}>
+            <Route path="/todos" element={<ProtectedRoute><Todos /></ProtectedRoute>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
